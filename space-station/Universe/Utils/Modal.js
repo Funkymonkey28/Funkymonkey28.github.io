4a11384f6bf3bf9ec1958ee0a678d5d2c19d1867
Funkymonkey28 <67377970+Funkymonkey28@users.noreply.github.com>
2022-10-13 14:41:43 +1100

    sidebar working

--- space-station/Universe/Utils/Modal.js
+++ space-station/Universe/Utils/Modal.js
@@ -8,6 +8,7 @@ export default class Modal {
       this.modalWindowOverlay.style.display = 'flex';
       document.getElementById("m-header").innerHTML = this.caller.getModalInfo()["header"];
       document.getElementById("m-content").innerHTML = this.caller.getModalInfo()["content"];
+      document.getElementById("universe-main").style.width = "75%";
     }
 
     this.caller.on("showInfo", showModalWindow);
@@ -16,6 +17,7 @@ export default class Modal {
     this.closeModalButton = document.getElementById("close-modal");
     const hideModalWindow = () => {
         this.modalWindowOverlay.style.display = 'none';
+        document.getElementById("universe-main").style.width = "100%";
     }
     this.closeModalButton.addEventListener("click", hideModalWindow);
     this.caller.on("closeInfo", hideModalWindow);
