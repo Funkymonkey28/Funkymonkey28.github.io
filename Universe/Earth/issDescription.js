6786f15fe5be4d4fbd48e5a8daf3350f1f77a395
Funkymonkey28 <67377970+N-Quan@users.noreply.github.com>
2022-11-29 15:28:06 +1100

    added descriptions

--- Universe/Earth/issDescription.js
+++ Universe/Earth/issDescription.js
@@ -1,23 +1,23 @@
 export default {
 		1: {
 			"name": "Zarya",
-			"content": "description"
+			"content": "Zarya, also known as the Functional Cargo Block or FGB, is the first module of the International Space Station to have been launched. The FGB provided electrical power, storage, propulsion, and guidance to the ISS during the initial stage of assembly."
 		},
 		2: {
 			"name": "Unity Node",
-			"content": "description"
+			"content": "The Unity connecting module, also known as Node 1, is the first U.S.-built component of the International Space Station. It connects the Russian and United States segments of the station, and is where crew eat meals together."
 		},
 		3: {
 			"name": "Pressurized Mating Adaptor 1",
-			"content": "description"
+			"content": "The Pressurized Mating Adapter (PMA) is a class of spacecraft adapters that convert the Common Berthing Mechanism (CBM) used on the US Orbital Segment to APAS-95 docking ports."
 		},
 		4: {
 			"name": "Pressurized Mating Adaptor 2",
-			"content": "description"
+			"content": "The Pressurized Mating Adapter (PMA) is a class of spacecraft adapters that convert the Common Berthing Mechanism (CBM) used on the US Orbital Segment to APAS-95 docking ports."
 		},
 		5: {
 			"name": "Zvezda Service Module",
-			"content": "description"
+			"content": "Zvezda Service Module, is a module of the International Space Station (ISS). It was the third module launched to the station, and provided all of the station's life support systems, some of which are supplemented in the US Orbital Segment (USOS), as well as living quarters for two crew members. It is the structural and functional center of the Russian Orbital Segment (ROS), which is the Russian part of the ISS. Crew assemble here to deal with emergencies on the station"
 		},
 		6: {
 			"name": "ITS-Z1",
@@ -25,162 +25,162 @@ export default {
 		},
 		7: {
 			"name": "Pressurized Mating Adaptor 3",
-			"content": "description"
+			"content": "The Pressurized Mating Adapter (PMA) is a class of spacecraft adapters that convert the Common Berthing Mechanism (CBM) used on the US Orbital Segment to APAS-95 docking ports."
 		},
 		8: {
-			"name": "???",
-			"content": "description"
+			"name": "Photovoltaic Arrays",
+			"content": "The International Space Station has four solar arrays, which produce more power than the station needs at one time for station systems. Altogether, the four sets of arrays can generate 84 to 120 kilowatts of electricity -- enough to provide power to more than 40 homes."
 		},
 		9: {
 			"name": "Destiny Space Laboratory",
-			"content": "description"
+			"content": "The Destiny module, also known as the U.S. Lab, is the primary operating facility for U.S. research payloads aboard the International Space Station. It was berthed to the Unity module and activated over a period of five days in February, 2001"
 		},
 		10: {
 			"name": "External Stowage Platform 1",
-			"content": "description"
+			"content": "External stowage platforms (ESPs) are key components of the International Space Station (ISS). Each platform is made from steel and serves as an external pallet that can hold spare parts, also known as orbital replacement units (ORUs), for the space station. As a platform it is not pressurized, but does require electricity to power the heaters of some of the stored equipment."
 		},
 		11: {
 			"name": "Canadarm2",
-			"content": "description"
+			"content": "Canadarm2 is part of Canada's contribution to the International Space Station (ISS). This 57.7-foot-long robotic arm was extensively involved in the assembly of the orbiting laboratory."
 		},
 		12: {
 			"name": "Quest Airlock",
-			"content": "description"
+			"content": "The Quest Joint Airlock, previously known as the Joint Airlock Module, is the primary airlock for the International Space Station. Quest was designed to host spacewalks with both Extravehicular Mobility Unit spacesuits and Orlan space suits. The airlock was launched on STS-104 on July 14, 2001."
 		},
 		13: {
 			"name": "Pirs Docking Compartment and Airlock",
-			"content": "description"
+			"content": "The Pirs Docking Compartment, also known as DC-1, is a Russian Module on the International Space Station. Pirs provides an airlock for cosmonaut spacewalks. It also serves as a docking port for transport and cargo vehicles to the Space Station. In addition, Pirs can transport fuel between the Zvezda or Zarya modules and docked vehicles such as a Soyuz or Progress spacecraft."
 		},
 		14: {
 			"name": "S0 ITS",
-			"content": "description"
+			"content": "The Integrated Truss Structure of the International Space Station consists of a linear arranged sequence of connected trusses on which various unpressurized components are mounted such as logistics carriers, radiators, solar arrays, and other equipment."
 		},
 		15: {
 			"name": "Mobile Base System",
-			"content": "description"
+			"content": "The Mobile Base System (MBS) is a work platform that moves on the Mobile Transporter rail car along truss rails covering the length of the space station. It provides a movable work platform for the Canadarm2 and Dextre as it traverses the main trusses to access any of eight worksites that feature power connections for the Base and any of its attachments."
 		},
 		16: {
 			"name": "Starboard HRS Radiator",
-			"content": "description"
+			"content": "The Heat Rejection System (HRS) maintains an equilibrium when the ISS environment or heat loads exceed the capabilities of the Passive Thermal Control System (PTCS)."
 		},
 		17: {
 			"name": "Port HRS Radiator",
-			"content": "description"
+			"content": "The Heat Rejection System (HRS) maintains an equilibrium when the ISS environment or heat loads exceed the capabilities of the Passive Thermal Control System (PTCS)."
 		},
 		18: {
 			"name": "External Stowage Compartment 2",
-			"content": "description"
+			"content": "External stowage platforms (ESPs) are key components of the International Space Station (ISS). Each platform is made from steel and serves as an external pallet that can hold spare parts, also known as orbital replacement units (ORUs), for the space station. As a platform it is not pressurized, but does require electricity to power the heaters of some of the stored equipment."
 		},
 		19: {
 			"name": "P3/P4 ITS",
-			"content": "description"
+			"content": "The Integrated Truss Structure of the International Space Station consists of a linear arranged sequence of connected trusses on which various unpressurized components are mounted such as logistics carriers, radiators, solar arrays, and other equipment."
 		},
 		20: {
 			"name": "Photovoltaic Arrays",
-			"content": "description"
+			"content": "The International Space Station has four solar arrays, which produce more power than the station needs at one time for station systems. Altogether, the four sets of arrays can generate 84 to 120 kilowatts of electricity -- enough to provide power to more than 40 homes."
 		},
 		21: {
 			"name": "P3/P4 ITS",
-			"content": "description"
+			"content": "The Integrated Truss Structure of the International Space Station consists of a linear arranged sequence of connected trusses on which various unpressurized components are mounted such as logistics carriers, radiators, solar arrays, and other equipment."
 		},
 		22: {
 			"name": "S3/S4 ITS",
-			"content": "description"
+			"content": "The Integrated Truss Structure of the International Space Station consists of a linear arranged sequence of connected trusses on which various unpressurized components are mounted such as logistics carriers, radiators, solar arrays, and other equipment."
 		},
 		23: {
-			"name": "???",
-			"content": "description"
+			"name": "Photovoltaic Arrays",
+			"content": "The International Space Station has four solar arrays, which produce more power than the station needs at one time for station systems. Altogether, the four sets of arrays can generate 84 to 120 kilowatts of electricity -- enough to provide power to more than 40 homes."
 		},
 		24: {
 			"name": "S3/S4 ITS",
-			"content": "description"
+			"content": "The Integrated Truss Structure of the International Space Station consists of a linear arranged sequence of connected trusses on which various unpressurized components are mounted such as logistics carriers, radiators, solar arrays, and other equipment."
 		},
 		25: {
 			"name": "External Stowage Compartment 3",
-			"content": "description"
+			"content": "External stowage platforms (ESPs) are key components of the International Space Station (ISS). Each platform is made from steel and serves as an external pallet that can hold spare parts, also known as orbital replacement units (ORUs), for the space station. As a platform it is not pressurized, but does require electricity to power the heaters of some of the stored equipment."
 		},
 		26: {
 			"name": "Harmony Node 2",
-			"content": "description"
+			"content": "Harmony, also known as Node 2, is the utility hub of the International Space Station. It connects the laboratory modules of the United States, Europe and Japan, as well as providing electrical power and electronic data. Sleeping cabins for four of the crew are housed here."
 		},
 		27: {
 			"name": "Columbus Space Laboratory",
-			"content": "description"
+			"content": "Columbus is a science laboratory that is part of the International Space Station and is the largest single contribution to the ISS made by the European Space Agency. Like the Harmony and Tranquility modules, the Columbus laboratory was constructed in Turin, Italy by Thales Alenia Space."
 		},
 		28: {
 			"name": "Kibo Space Laboratory Pressurized Stowage Module",
-			"content": "description"
+			"content": "Stowage module for the Kibo Space Laboratory"
 		},
 		29: {
 			"name": "DEXTRE",
-			"content": "description"
+			"content": "Dextre, also known as the Special Purpose Dexterous Manipulator, is a two armed robot, or telemanipulator, which is part of the Mobile Servicing System on the International Space Station, and does repairs that would otherwise require astronauts to do spacewalks."
 		},
 		30: {
 			"name": "Kibo Space Laboratory Presurrized Module",
-			"content": "description"
+			"content": "he Japanese Experiment Module (JEM), nicknamed Kibō (きぼう, Kibō, Hope), is a Japanese science module for the International Space Station (ISS) developed by JAXA. It is the largest single ISS module, and is attached to the Harmony module. The first two pieces of the module were launched on Space Shuttle missions STS-123 and STS-124."
 		},
 		31: {
 			"name": "Robotic Arm",
-			"content": "description"
+			"content": "Launched to the ISS in 2001, it plays a key role in station assembly and maintenance; it moves equipment and supplies around the station, supports astronauts working in space, and services instruments and other payloads attached to the ISS and is used for external maintenance."
 		},
 		32: {
-			"name": "???",
-			"content": "description"
+			"name": "Photovoltaic Arrays",
+			"content": "The International Space Station has four solar arrays, which produce more power than the station needs at one time for station systems. Altogether, the four sets of arrays can generate 84 to 120 kilowatts of electricity -- enough to provide power to more than 40 homes."
 		},
 		33: {
 			"name": "Kibo Space Laboratory Exposed Platform",
-			"content": "description"
+			"content": "The Exposed Facility, or EF, is a unique platform on the ISS that is located outside of the Pressurized Module and is continuously exposed to the space environment. Astronauts exchange experiment payloads or hardware from the Pressurized Module through the scientific airlock using the Kibo Remote Manipulator System."
 		},
 		34: {
 			"name": "Poisk Mini Research Module",
-			"content": "description"
+			"content": "Poisk is a docking module of the International Space Station. It has two egress hatches for EVAs in addition to the two spacecraft docking ports."
 		},
 		35: {
 			"name": "Express Logisitics Carrier 1",
-			"content": "description"
+			"content": "An EXpedite the PRocessing of Experiments to Space Station (ExPRESS) Logistics Carrier (ELC) is an unpressurized attached payload platform for the International Space Station (ISS) that provides mechanical mounting surfaces, electrical power, and command and data handling services for Orbital Replacement Units (ORUs) as well as science experiments on the ISS."
 		},
 		36: {
 			"name": "Express Logisitics Carrier 2",
-			"content": "description"
+			"content": "An EXpedite the PRocessing of Experiments to Space Station (ExPRESS) Logistics Carrier (ELC) is an unpressurized attached payload platform for the International Space Station (ISS) that provides mechanical mounting surfaces, electrical power, and command and data handling services for Orbital Replacement Units (ORUs) as well as science experiments on the ISS."
 		},
 		37: {
 			"name": "Tranquility Node 2",
-			"content": "description"
+			"content": "Tranquility is a module of the International Space Station. It contains environmental control systems, life support systems, a toilet, exercise equipment, and an observation cupola. The European Space Agency and the Italian Space Agency had Tranquility manufactured by Thales Alenia Space."
 		},
 		38: {
 			"name": "Cupola",
-			"content": "description"
+			"content": "The Cupola is an ESA-built observatory module of the International Space Station (ISS). Its name derives from the Italian word cupola, which means dome. Its seven windows are used to conduct experiments, dockings and observations of Earth."
 		},
 		39: {
 			"name": "Rassvet Mini Research Module",
-			"content": "description"
+			"content": "Rassvet is primarily used for cargo storage and as a docking port for visiting spacecraft. It was flown to the ISS aboard Space Shuttle Atlantis on the STS-132 mission on 14 May 2010,[2] and was connected to the ISS on 18 May 2010."
 		},
 		40: {
 			"name": "Leonardo Raffaello Perm Multi Purpose Logistics Module",
-			"content": "description"
+			"content": "The Leonardo MPLM, also known as MPLM-1, was one of three Multi-Purpose Logistics Modules which were operated by NASA to transfer supplies and equipment to and from the International Space Station. Leonardo was used for eight of the twelve MPLM flights to the space station, with Raffaello being used for the other four."
 		},		
 		41: {
 			"name": "Express Logistics Carrier 4",
-			"content": "description"
+			"content": "An EXpedite the PRocessing of Experiments to Space Station (ExPRESS) Logistics Carrier (ELC) is an unpressurized attached payload platform for the International Space Station (ISS) that provides mechanical mounting surfaces, electrical power, and command and data handling services for Orbital Replacement Units (ORUs) as well as science experiments on the ISS."
 		},
 		42: {
 			"name": "Alpha Magnetic Spectrometer",
-			"content": "description"
+			"content": "The Alpha Magnetic Spectrometer (AMS-02) is a particle physics experiment module that is mounted on the International Space Station (ISS). The experiment is a recognized CERN experiment (RE1). The module is a detector that measures antimatter in cosmic rays; this information is needed to understand the formation of the Universe and search for evidence of dark matter."
 		},
 		43: {
 			"name": "Express Logistics Carrier 3",
-			"content": "description"
+			"content": "An EXpedite the PRocessing of Experiments to Space Station (ExPRESS) Logistics Carrier (ELC) is an unpressurized attached payload platform for the International Space Station (ISS) that provides mechanical mounting surfaces, electrical power, and command and data handling services for Orbital Replacement Units (ORUs) as well as science experiments on the ISS."
 		},
 		44: {
 			"name": "Enhanced ISS Boom Assembly",
-			"content": "description"
+			"content": "The sensor package has been unpowered and unheated for seven years, since STS-134, and is most likely dead.  The IBA would be used to extend the reach of the SSRMS if another problem happened out at the end of a solar array or the like."
 		},
 		45: {
 			"name": "RapidScat",
-			"content": "description"
+			"content": "ISS-RapidScat was an instrument mounted to the International Space Station's Columbus module that measured wind speeds. It was launched aboard SpaceX CRS-4 in September 2014 and operated until August 2016. ISS-RapidScat was a scatterometer designed to support weather forecasting by bouncing microwaves off the ocean's surface to measure wind speed via wind waves."
 		},
 		46: {
 			"name": "Photovoltaic Radiators",
-			"content": "description"
+			"content": "The Photovoltaic Radiators (PVR) on the ISS are responsible for radiating into space the waste heat produced by the photovoltaic power system (solar panels and associated electronics). <br><br> The PVRs are passive devices which themselves contain no photovoltaic technology."
 		},
 }
